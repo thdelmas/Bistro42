5a35c8f942f2c91826109d3ddd68e26b343638ee
jerry <jerry@pop-os.localdomain>
2021-04-22 23:29:17 +0200

    add du shell et des  vms

---
+++ generate_vm.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+#     This is a script made for titou and you guys !
+# 42Home the nearest 42 campus through your bed
+#
+# Let's discover the Peer2Home ! (^_^?)
+#
+# Sweetly yours
+# Sweety
+
+VM_NAME='42Home'
+VM_FOLDER="$HOME"'/VirtualBox VMs'
+VM_RAM="$((1024*2))"
+VM_VRAM='128'
+
+VM_HDD_SIZE="$((1024*16))"
+
+# Don't touch
+VM_OS_TYPE='Ubuntu_64'
+ISO_NAME='xubuntu-20.10-desktop-amd64.iso'
+
+
+#0. Say: Hi !
+echo "$VM_NAME"' X Bistro42'
+echo "Welcome $USER !"
+echo 'Creating VirtualBox VM...'
+
+
+#1. Create VirtualMachine inside VirtualBox with VBoxManage the CLI
+VBoxManage createvm --name "$VM_NAME" --ostype "$VM_OS_TYPE" --register
+
+
+#2. Configure Basic system settings (Network, graphics & memory)
+
+# http://www.virtualbox.org/manual/ch03.html#settings-motherboard
+VBoxManage modifyvm "$VM_NAME" --ioapic on                     
+
+VBoxManage modifyvm "$VM_NAME" --memory "$VM_RAM" --vram "$VM_VRAM"
+VBoxManage modifyvm "$VM_NAME" --graphicscontroller vmsvga
+VBoxManage modifyvm "$VM_NAME" --nic1 nat
+
+
+#3. Create HDD
+VBoxManage createhd --filename "${VM_FOLDER}/${VM_NAME}/${VM_NAME}_DISK.vdi" --size "$VM_HDD_SIZE"  --format VDI                     
+VBoxManage storagectl "$VM_NAME" --name "SATA Controller" --add sata --controller IntelAhci       
+VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "${VM_FOLDER}/${VM_NAME}/${VM_NAME}_DISK.vdi"
+
+#4. Get and attach ISO
+if ! [ -e "xubuntu-20.10-desktop-amd64.iso" ]
+then
+	echo 'Downloading xubuntu 20.10 LTS ...'
+	wget 'http://ftp.free.fr/mirrors/ftp.xubuntu.com/releases/20.10/release/xubuntu-20.10-desktop-amd64.iso'
+	echo 'Now launch it again, haha !'
+else
+VBoxManage storagectl "$VM_NAME" --name "IDE Controller" --add ide --controller PIIX4       
+VBoxManage storageattach "$VM_NAME" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "$(pwd)/$ISO_NAME"
+VBoxManage modifyvm "$VM_NAME" --boot1 dvd --boot2 disk --boot3 none --boot4 none 
+
+# Insert Face B or Disk 2 to finish the install (^_^?)
+fi
