3f51a8f809b87490e1780313f09e637c557c81d4
Theophile DELMAS <thdelmas@e2r2p4.42.fr>
2021-04-24 13:52:37 +0200

    feat 2 proc et pf

--- generate_vm.sh
+++ generate_vm.sh
@@ -14,10 +14,11 @@ VM_RAM="$((1024*2))"
 VM_VRAM='128'
 
 VM_HDD_SIZE="$((1024*16))"
+VM_CPUS_COUNT='2'
 
 # Don't touch
 VM_OS_TYPE='Ubuntu_64'
-ISO_NAME='xubuntu-20.10-desktop-amd64.iso'
+VM_ISO='42Home.iso'
 
 
 #0. Say: Hi !
@@ -38,6 +39,8 @@ VBoxManage modifyvm "$VM_NAME" --ioapic on
 VBoxManage modifyvm "$VM_NAME" --memory "$VM_RAM" --vram "$VM_VRAM"
 VBoxManage modifyvm "$VM_NAME" --graphicscontroller vmsvga
 VBoxManage modifyvm "$VM_NAME" --nic1 nat
+VBoxManage modifyvm "$VM_NAME" --natpf1 "ssh,tcp,,4222,,22"
+VBoxManage modifyvm "$VM_NAME" --cpus "$VM_CPUS_COUNT"
 
 
 #3. Create HDD
@@ -46,14 +49,14 @@ VBoxManage storagectl "$VM_NAME" --name "SATA Controller" --add sata --controlle
 VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "${VM_FOLDER}/${VM_NAME}/${VM_NAME}_DISK.vdi"
 
 #4. Get and attach ISO
-if ! [ -e "xubuntu-20.10-desktop-amd64.iso" ]
+if ! [ -e "$VM_ISO" ]
 then
 	echo 'Downloading xubuntu 20.10 LTS ...'
 	wget 'http://ftp.free.fr/mirrors/ftp.xubuntu.com/releases/20.10/release/xubuntu-20.10-desktop-amd64.iso'
 	echo 'Now launch it again, haha !'
 else
 VBoxManage storagectl "$VM_NAME" --name "IDE Controller" --add ide --controller PIIX4       
-VBoxManage storageattach "$VM_NAME" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "$(pwd)/$ISO_NAME"
+VBoxManage storageattach "$VM_NAME" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "$(pwd)/$VM_ISO"
 VBoxManage modifyvm "$VM_NAME" --boot1 dvd --boot2 disk --boot3 none --boot4 none 
 
 # Insert Face B or Disk 2 to finish the install (^_^?)
