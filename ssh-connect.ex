0a1d864507dceba11059dbaeeb75b68bc39c75b2
Theophile DELMAS <thdelmas@e1r12p2.42.fr>
2021-05-27 12:56:16 +0200

    add: auto-scp ssh-key

---
+++ ssh-connect.ex
@@ -0,0 +1,15 @@
+#!/usr/bin/expect -f
+#  ./ssh.exp port ip user password
+#  ./ssh.exp 4222 127.0.0.1 xubuntu xubuntu
+
+set sshport [lrange $argv 0 0]
+set server [lrange $argv 1 1]
+set user [lrange $argv 2 2]
+set pass [lrange $argv 3 3]
+
+spawn scp -P$sshport -pr .ssh $user@$server:/home/$user/
+match_max 100000
+expect "password: $"
+send -- "$pass\r"
+send -- "\r"
+interact
